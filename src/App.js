df172cdf4e3862d8baffff55e4c664222996b6d8
rohittendulkar <vrekedit00@gmail.com>
2022-09-19 16:59:07 +0530

    sigsi assessment completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import LatestNews from "./components/LatestNews";
+import Navigation from "./components/Navigation";
+import TravelSection from "./components/TravelSection";
+import WeatherComponent from "./components/WeatherComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Navigation />
+			<HeroSection />
+			<LatestNews />
+			<TravelSection />
+			<WeatherComponent />
+		</>
+	);
 }
 
 export default App;
